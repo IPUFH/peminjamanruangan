8a5b4385a7965d7f434f463627bbd1c6130424ad
zamil <zamilkunn@gmail.com>
2024-04-15 19:52:53 +0700

    Update
    Update

---
+++ js/login.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Periksa status login saat halaman login dimuat
+    checkLoginStatus();
+
+    document.getElementById("login-form").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        // Ambil nilai input username dan password
+        const usernameInput = document.getElementById("username").value;
+        const passwordInput = document.getElementById("password").value;
+
+        // ID dan password sederhana untuk keperluan rekrayasa
+        const sampleUsername = "admin";
+        const samplePassword = "IPUFH";
+
+        // Validasi username dan password
+        if (validateLogin(usernameInput, passwordInput, sampleUsername, samplePassword)) {
+            // Cek apakah peringatan login berhasil sudah ditampilkan sebelumnya
+            const loginSuccessAlertShown = localStorage.getItem("loginSuccessAlertShown");
+
+            if (!loginSuccessAlertShown) {
+                // Tampilkan peringatan login berhasil sekali saja
+                alert("Login berhasil");
+                localStorage.setItem("loginSuccessAlertShown", "true");
+            }
+
+            // Contoh redirect ke halaman lain (dasboar.html) setelah login berhasil
+            window.location.href = "dasboard.html";
+            localStorage.setItem("isLoggedIn", "true");
+        } else {
+            // Tampilkan pesan error jika login gagal
+            alert("Username atau password salah");
+        }
+    });
+});
+
+
+function validateLogin(username, password, sampleUsername, samplePassword) {
+    // Periksa apakah username dan password sesuai dengan contoh
+    return username === sampleUsername && password === samplePassword;
+}
+
+function checkLoginStatus() {
+    // Cek apakah ada status login sebelumnya
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+}
+
